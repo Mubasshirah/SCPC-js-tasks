66de49f81bac8207c2f2f66e38ab728fbb912c6a
Mubasshirah <adibaju1994@gmail.com>
2023-07-27 23:49:03 +0600

    last commit

--- task.js
+++ task.js
@@ -1,29 +1,29 @@
 // Task:1
 
-// let newString=""
-// function reverseStr(string){
-//     for(let i=string.length-1;i>=0;i--){
-//         newString +=string[i];
-//     }
-//     return newString;
-// }
-// const exemple=reverseStr('hello world');
-// console.log(exemple);
+let newString=""
+function reverseStr(string){
+    for(let i=string.length-1;i>=0;i--){
+        newString +=string[i];
+    }
+    return newString;
+}
+const exemple=reverseStr('hello world');
+console.log(exemple);
 
 // Task:2
 // 
-// let sum=0;
-// function sumOfEven(number){
-//     for(let i=0;i<number.length;i++){
-//         if(number[i]>0){
+let sum=0;
+function sumOfEven(number){
+    for(let i=0;i<number.length;i++){
+        if(number[i]>0){
 
-//             sum +=number[i];
-//         }
-//     }
-//     return sum;
-// }
-// const number=[2, -5, 10, -3, 7]
-// console.log(sumOfEven(number))
+            sum +=number[i];
+        }
+    }
+    return sum;
+}
+const number=[2, -5, 10, -3, 7]
+console.log(sumOfEven(number))
 
 // Task:3
 
@@ -48,3 +48,38 @@ function frequentElement(array){
 }
 
 console.log(frequentElement(array))
+
+// task:4
+function findTwoNumber(array, target) {
+    let leftIndex = 0;
+    let rightIndex = array.length - 1;
+  
+    if (leftIndex < rightIndex) {
+      const currentSum = array[leftIndex] + array[rightIndex];
+  
+      if (currentSum === target) {
+        return [left, right];
+      } else if (currentSum < target) {
+        leftIndex++;
+      } else {
+        rightIndex--;
+      }
+    }
+  
+   return null
+    
+  }
+  
+  
+  const sortedArray = [1, 3, 6, 8, 11, 15];
+  const targetValue = 12;
+  const result = findTwoNumber(sortedArray, targetValue);
+ 
+  
+    console.log(result)
+
+// task:5
+
+
+
+
